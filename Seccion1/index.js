c612022c352a8ae7801fe7345b9304c3addb979e
daniel <daniel.lopez@escotel.com.mx>
2025-05-19 17:59:15 -0600

    Curso css

---
+++ Seccion1/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+
+export const index = () => {
+  const [nombre, setNombre] = useState('');
+
+  useEffect(() => {
+      
+  }, []);
+  
+
+  return (
+    <div>index</div>
+  )
+}
